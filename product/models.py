dd2cac8d8f63a9f4f134eb1047063669a4c9f86e
smyucel <selimmertyucel@gmail.com>
2025-04-17 02:51:21 +0300

    Database Olusturuldu

--- product/models.py
+++ product/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayir'),
+    )
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    image = models.ImageField(blank=True,upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
