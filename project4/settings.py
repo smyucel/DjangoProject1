824d934f808c973c69cdc2ebc89f99a7df50ffb2
smyucel <selimmertyucel@gmail.com>
2025-04-24 00:24:28 +0300

    Resim Galerisi ve Admin panelinde Image calistirma

--- project4/settings.py
+++ project4/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # Default primary key field type
